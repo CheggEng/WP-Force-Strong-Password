b0f14dffd797c6a1b3be5439da9041c23a955297
Aaron Collins <acollins@chegg.com>
2015-10-12 14:48:46 -0700

    use php ds constant

--- Class/ForceStrongPassword.php
+++ Class/ForceStrongPassword.php
@@ -2,7 +2,7 @@
 
 class ForceStrongPassword{
   static function UserEditTemplate($profileuser){
-      include_once(FORCE_STRONG_PASSWORD.DS.'Template'.DS.'user-edit.php');
+      include_once(FORCE_STRONG_PASSWORD.DIRECTORY_SEPARATOR.'Template'.DIRECTORY_SEPARATOR.'user-edit.php');
       return false;
   }
 }
--- register.php
+++ register.php
@@ -1,7 +1,8 @@
 <?php
 /*
 Plugin Name: Force Strong Password
-Plugin URI: hWordpress 4.3+ now encourages using strong passwords but it still gives the user a option to override the strong password.  This simple plugin takes that option away.
+Plugin URI: https://github.com/CheggEng/WP-Force-Strong-Password
+Description: Wordpress 4.3+ now encourages using strong passwords but it still gives the user a option to use a weak password if they click confirm use of a weak password. This simple plugin takes away the option to allow weak password override. The end result is a wordpress site that assures strong passwords are used.
 Author: Aaron Collins <acollins@chegg.com>
 Version: 1.0
 Author URI: http://www.chegg.com
@@ -9,5 +10,5 @@ License: GPLv2
 */
 
 define('FORCE_STRONG_PASSWORD',plugin_dir_path(__FILE__));
-include_once(FORCE_STRONG_PASSWORD.DS.'Class'.DS.'ForceStrongPassword.php');
-add_filter('show_password_fields', ForceStrongPassword::UserEditTemplate);
+include_once(FORCE_STRONG_PASSWORD.DIRECTORY_SEPARATOR.'Class'.DIRECTORY_SEPARATOR.'ForceStrongPassword.php');
+add_filter('show_password_fields', 'ForceStrongPassword::UserEditTemplate');
